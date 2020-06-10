946aad56743b26092504dc0f8f296c681de1eca2
Abdolsalam Dehvari <asd.dehvari@gmail.com>
2020-06-10 13:26:55 +0430

    Updates

---
+++ precache-manifest.c204ce287ed86eaef719c12eca6f204b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3120e5739d29315c7d98ac11ddf2a02b",
+    "url": "/tictoktoe/index.html"
+  },
+  {
+    "revision": "3baeb2bd015bdc4e9062",
+    "url": "/tictoktoe/static/css/main.d3f772cf.chunk.css"
+  },
+  {
+    "revision": "c4732bb5a08aba9aff91",
+    "url": "/tictoktoe/static/js/2.33079719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictoktoe/static/js/2.33079719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3baeb2bd015bdc4e9062",
+    "url": "/tictoktoe/static/js/main.2eb4a58d.chunk.js"
+  },
+  {
+    "revision": "3ade165f0bd0f2d68c54",
+    "url": "/tictoktoe/static/js/runtime-main.6976b8a9.js"
+  }
+]);
